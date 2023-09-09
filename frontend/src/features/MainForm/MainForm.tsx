d2a3baf0bbbd14b045612fdbddd20f059b1b0c61
hallucinogenizer <22100063@lums.edu.pk>
2023-09-09 18:48:35 +0500

    added titles input separated by commas

--- frontend/src/features/MainForm/MainForm.tsx
+++ frontend/src/features/MainForm/MainForm.tsx
@@ -12,7 +12,32 @@ import FormInput from "@/components/FormInput/FormInput";
 
 
 const schema = yup.object({
-  name: yup.string().required().min(3).max(40)
+  name: yup.string().required().min(3).max(40),
+  titles: yup
+  .string()
+  .required('Titles are required')
+  .test(
+    'at-least-two-items',
+    'There must be at least two items separated by a comma',
+    (value) => {
+      if (!value) return false; // If the input is empty, the validation fails
+      const items = value.split(',').map((item) => item.trim());
+
+      // Check if there are at least two items and none of them are empty
+      return items.length >= 2;
+    }
+  )
+  .test(
+    'no-empty-item',
+    'There must be an item after every comma. Make sure there is no empty comma at the end of your input',
+    (value) => {
+      if (!value) return false; // If the input is empty, the validation fails
+      const items = value.split(',').map((item) => item.trim());
+
+      // Check if there are at least two items and none of them are empty
+      return items.every((item) => item !== '');
+    }
+  ),
 }).required();
 
 
@@ -32,6 +57,7 @@ export default function MainForm() {
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
         <FormInput name="name" label="Full Name: " />
+        <FormInput name="titles" label="Professional Titles: " />
         <Button type="submit">Submit</Button>
       </form>
     </Form>
